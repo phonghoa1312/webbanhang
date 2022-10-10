81b300780a44f13b890ee6c6126b6620830892d6
phonghoa1312 <phonghoa1312@gmail.com>
2022-10-10 15:58:53 +0700

    commit

---
+++ src/main/java/com/example/web_banhang/WebBanhangApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_banhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanhangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBanhangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web_banhang/WebBanhangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_banhang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanhangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
